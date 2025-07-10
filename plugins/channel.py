a9e4e6ea16d79ec77b8a4f209d0b65e72bf76269
Sourav0879 <145244763+Sourav0879@users.noreply.github.com>
2025-07-10 14:08:07 +0600

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -50,7 +50,7 @@ UPDATE_CAPTION = """<b>𝖭𝖤𝖶 {} 𝖠𝖣𝖣𝖤𝖣 ✅</b>
 
 {}
 
-<blockquote>🌿 Lɪɴᴋ Fᴏʀ Rᴏꜱᴇ Bᴏᴛ : <a href="https://telegram.me/rosemovieaibot?start=getfile-{file_name}">𝐑𝐨𝐬𝐞 𝐀𝐢 𝐌𝐨𝐯𝐢𝐞 𝐁𝐨𝐭 &lt;/&gt;</a></blockquote>
+<blockquote>🌿 Lɪɴᴋ Fᴏʀ Rᴏꜱᴇ Bᴏᴛ : <a href="https://telegram.me/rosemovieaibot?start=getfile-{file_name}">𝐑𝐨𝐬𝐞 𝐀𝐢 𝐌𝐨𝐯𝐢𝐞 𝐁𝐨𝐭</a></blockquote>
 
 <blockquote>〽️ Powered by @LuxeFlix</b></blockquote>"""
 
